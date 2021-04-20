6fa2d0d8974d400629fb454a0759d284afc98ac7
JunmingGuo <sjunmg@gmail.com>
2021-04-19 21:38:59 -0700

    Implement addToCart in Product component

--- shopping-cart/src/components/Product.tsx
+++ shopping-cart/src/components/Product.tsx
@@ -1,25 +1,46 @@
+import { useContext, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { ProductsContext } from '../context';
+import { Product as ProductType } from '../types';
 
 interface Props {
-  product: {
-    id: string;
-    title: string;
-    price: number;
-    url: string;
-  };
-};
+  product: ProductType
+}
 
 const Product = ({ product }: Props) => {
-  const { id, title, price, url } = product;
+  const { id } = product.sys;
+  const { title, price } = product.fields;
+  const { url } = product.fields.image.fields.file;
+  const { addToCart, cartItem } = useContext(ProductsContext);
+  const [isItemAdded, setIsItemAdded] = useState(false);
+
+  const handleAddtoCart = () => {
+    const item = { ...product, count: 1 };
+    if (addToCart) {
+      addToCart(item);
+    }
+  };
+
+  useEffect(() => {
+    const checkIsItemAdded = () => {
+      const itemFound = cartItem?.find(item => item.sys.id === id);
+      if (itemFound) {
+        setIsItemAdded(true);
+      } else {
+        setIsItemAdded(false);
+      }
+    }
+    checkIsItemAdded();
+  }, [cartItem, id]);
 
   return (
     <article className="product">
       <div className="img-container">
         <img className="product-img" src={url} alt={title} />
-        <button className="bag-btn" data-id={id}>
+        <button className="bag-btn" onClick={handleAddtoCart} disabled={isItemAdded}>
           <FontAwesomeIcon icon={faShoppingCart} />
-          add to bag
+          {isItemAdded ? 'added to bag' : 'add to bag'}
         </button>
       </div>
       <h3>{title}</h3>
@@ -29,3 +50,4 @@ const Product = ({ product }: Props) => {
 };
 
 export default Product;
+
--- shopping-cart/src/components/ProductList.tsx
+++ shopping-cart/src/components/ProductList.tsx
@@ -2,25 +2,11 @@ import { useContext } from 'react';
 import Product from './Product';
 import { ProductsContext } from '../context';
 
-interface ProductInfo {
-  id: string;
-  title: string;
-  price: number;
-  url: string;
-}
-
 const ProductList = () => {
   const { products } = useContext(ProductsContext);
   
   const productList = products && products.map(product => {
-    const productInfo: ProductInfo = {
-      id: product.sys.id,
-      title: product.fields.title,
-      price: product.fields.price,
-      url: product.fields.image.fields.file.url
-    };
-
-    return <Product key={productInfo.id} product={productInfo} />;
+    return <Product key={product.sys.id} product={product} />;
   });
 
   return (
